a9675e25546e8950525d3ab167eeb95b2056b658
c0nf1g <zhala@protonmail.com>
2022-06-26 14:53:09 +0300

    updated initial configuration

--- src/flashcards/__init__.py
+++ src/flashcards/__init__.py
@@ -1,18 +1,24 @@
 from flask import Flask
+from flask_bcrypt import Bcrypt
+from flask_cors import CORS
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
 
 from .config import get_config
 
+cors = CORS()
 db = SQLAlchemy()
 migrate = Migrate()
+bcrypt = Bcrypt()
 
 
 def create_app(config_name):
-    app = Flask('flashcards')
+    app = Flask("flashcards")
     app.config.from_object(get_config(config_name))
 
+    cors.init_app(app)
     db.init_app(app)
     migrate.init_app(app)
+    bcrypt.init_app(app)
 
     return app
